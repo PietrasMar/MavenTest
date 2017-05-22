d228a9b7c418b9a296166d51fd3e5095e11bca2e
Marcin <mrcn.pietrasiak@gmail.com>
2017-05-22 21:09:15 +0200

    New Test for int=0;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int N =2;
+        int N=2;
         Solution sol= new Solution();
         System.out.println(sol.solution(N));
 
--- src/main/java/SolutionTest.java
+++ src/main/java/SolutionTest.java
@@ -6,10 +6,11 @@ public class SolutionTest {
     public void testSolution() throws Exception {
         Solution solTest = new Solution();
 
+        assertEquals(1,solTest.solution(0));
         assertEquals(5,solTest.solution(1));
         assertEquals(13,solTest.solution(2));
         assertEquals(29,solTest.solution(3));
-
+        assertEquals(-1,solTest.solution(19990));
 
     }
 }
